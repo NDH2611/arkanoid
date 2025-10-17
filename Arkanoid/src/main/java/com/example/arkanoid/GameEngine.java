2a72e2472e6d168726dc291169ca40c9c886d8ab
Hisuuz06 <24021474@vnu.edu.vn>
2025-10-17 12:01:51 +0700

    Level

--- Arkanoid/src/main/java/com/example/arkanoid/GameEngine.java
+++ Arkanoid/src/main/java/com/example/arkanoid/GameEngine.java
@@ -15,23 +15,13 @@ public class GameEngine {
     public static final int HEIGHT = 600;
     public static final double BALL_SPEED = 3.0;
 
-    public static int height = 40;
-    public static int width = 80;
-    public static int space = 5;
-    public static int columns = 9;
-    public static int rows = 5;
-    public static int distanceY = 10;
-    public static int distanceX = (800 - width * columns - space * (columns - 1)) / 2;
-    public static int bricksType = 5;
-
     private Canvas canvas;
     private GraphicsContext gc;
     private Scene scene;
 
     private Ball ball;
     private Paddle paddle;
-
-    private final static ArrayList<Brick> bricks = new ArrayList<>();
+    private ArrayList<Level>  levels =  new ArrayList<>();
 
     public GameEngine() {
         initialize();
@@ -43,7 +33,8 @@ public class GameEngine {
         gc = canvas.getGraphicsContext2D();
         ball = new Ball(WIDTH / 2, HEIGHT / 2, 10, BALL_SPEED);
         paddle = new Paddle(WIDTH / 2, HEIGHT * 3 / 4, 75, 25, 0);
-        showBricks();
+        //showBricks();
+        createLevel();
 
         StackPane root = new StackPane(canvas);
         scene = new Scene(root, WIDTH, HEIGHT);
@@ -64,8 +55,13 @@ public class GameEngine {
         gameLoop.start();
     }
 
-    private void renderUpdateBrick() {
-        for (Brick brick : bricks) {
+    private void createLevel() {
+        Level level = new Level();
+        levels.add(level);
+    }
+
+    private void renderUpdateLevel() {
+        for (Brick brick : levels.get(0).getBricks()) {
             if (!brick.isVisible()) {
                 continue;
             }
@@ -92,7 +88,7 @@ public class GameEngine {
         } else if (side == CheckCollision.CollisionSide.LEFT || side == CheckCollision.CollisionSide.RIGHT) {
             ball.setDx(-ball.getDx());
         }
-        renderUpdateBrick();
+        renderUpdateLevel();
         paddle.update();
     }
 
@@ -100,37 +96,7 @@ public class GameEngine {
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         ball.render(gc);
         paddle.render(gc);
-        for (Brick brick : bricks) {
-            brick.render(gc);
-        }
-    }
-
-    public static void newBricks() {
-        bricks.clear();
-        for (int i = 0; i < columns; i++) {
-            for (int j = 0; j < rows; j++) {
-                int x = i * (width + space) + distanceX;
-                int y = j * (height + space) + distanceY;
-                int type = (int) (Math.random() * bricksType) + 1;
-                Brick brick = new Brick(x, y, width, height);
-                if (type == 1) {
-                    brick.setType(Brick.TYPE.GREEN);
-                } else if (type == 2) {
-                    brick.setType(Brick.TYPE.ORANGE);
-                } else if (type == 3) {
-                    brick.setType(Brick.TYPE.YELLOW);
-                } else if (type == 4) {
-                    brick.setType(Brick.TYPE.PURPLE);
-                } else {
-                    brick.setType(Brick.TYPE.PINK);
-                }
-                bricks.add(brick);
-            }
-        }
-    }
-
-    public void showBricks() {
-        newBricks();
+        levels.get(0).render(gc);
     }
 
     public void handleKeyInput(KeyEvent event) {
--- Arkanoid/src/main/java/com/example/arkanoid/PowerUp.java
+++ Arkanoid/src/main/java/com/example/arkanoid/PowerUp.java
@@ -3,6 +3,7 @@ package com.example.arkanoid;
 public class PowerUp extends GameObject{
     private static final double FALL_SPEED = 1.5;
     private double duration;
+    private boolean active;
 
     public PowerUp(double x, double y, double width, double height, double duration) {
         super(x, y, width, height);
